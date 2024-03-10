2bae195be63434f348ab39ee44efc5cadab9b12e
Ahmed <63443985+dev1942@users.noreply.github.com>
2024-03-10 17:19:24 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://nvavawqwasqoushxsfxl.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52YXZhd3F3YXNxb3VzaHhzZnhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDgwMDIsImV4cCI6MjAyMjg4NDAwMn0.yzT-VWasbfxSCXGF-C_TOWiiyVs6Sbob9yJEvcKWp0g',
+    supabaseUrl: 'https://yvuujozmolvnblscwydv.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dXVqb3ptb2x2bmJsc2N3eWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAwNzI3NDYsImV4cCI6MjAyNTY0ODc0Nn0.tx-GoY9dgW7xHekJdonlPN9HVQbV7AY3C2YDkOxmamM',
 }
